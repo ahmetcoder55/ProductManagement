9a240d85775240eceb312067dd1b6bbc46d7235d
ahmetcoder55 <244322943+ahmetcoder55@users.noreply.github.com>
2026-09-08 21:38:11 +0300

    The Generic Repository has been implemented.

---
+++ ProductManagement.DataAccess/Abstract/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using ProductManagement.Entities.Abstract;
+using ProductManagement.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ProductManagement.DataAccess.Abstract
+{
+    public interface IGenericRepository<T> where T:BaseEntity,IEntity
+    {
+        Task<List<T>> GetAllAsync();
+        Task<List<T>> GetWhereAsync(Expression<Func<T, bool>> predicate);
+        Task<T?> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ ProductManagement.DataAccess/Concrete/GenericRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagement.DataAccess.Abstract;
+using ProductManagement.Entities.Abstract;
+using ProductManagement.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ProductManagement.DataAccess.Concrete
+{
+    public class GenericRepository<TEntity, TContext> : IGenericRepository<TEntity>
+     where TEntity : BaseEntity, IEntity
+        where TContext:DbContext
+    {
+
+        //Dependency Injection
+        protected readonly TContext _context;
+
+        public GenericRepository(TContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+        }
+
+        public async Task<List<TEntity>> GetAllAsync()
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<TEntity?> GetByIdAsync(int id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task<List<TEntity>> GetWhereAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _context.Set<TEntity>().Where(predicate).ToListAsync();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+        }
+    }
+}
